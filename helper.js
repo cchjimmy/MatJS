c067f817e32070243516fd9135da508a4b7a4d3d
himhimjimmy@yahoo.com.hk <himhimjimmy@yahoo.com.hk>
2023-04-25 17:06:02 +0100

    tidy up code

---
+++ helper.js
@@ -0,0 +1,5 @@
+export function measureFunc(func) {
+    let past = performance.now();
+    func();
+    return performance.now() - past;
+  }
--- main.js
+++ main.js
@@ -23,27 +23,7 @@ import Mat from './Mat.js';
     canvas.width = 400;
     canvas.height = 400;
 
-    let a, o, g, ot, gt;
-    a = Mat.random(6, 6);
-    ot = measureFunc(() => o = Mat.det(a, 'old'));
-    gt = measureFunc(() => g = Mat.det(a));
-    let txt = `<h2>Mat.det() performance test</h2>Input matrix:<pre><code>${Mat.string(a)}</code></pre>Time taken:<br>old: ${ot} ms<br>gauss: ${gt} ms<br><br>Determinants:<br>From old method: ${o}<br>Incorporating gauss elimination: ${g}<br><br>Percentage difference (old - gauss): ${(o - g) * 100} %`;
-
-    let div = document.createElement('div');
-    div.innerHTML = txt;
-    div.style.color = 'white';
-    document.body.appendChild(div);
-
-    console.log(measureFunc(() => Mat.copy(a)), 'ms');
-    console.log(measureFunc(() => Mat.matrix(...a)), 'ms');
-
     update();
-  }
-
-  function measureFunc(func) {
-    let past = performance.now();
-    func();
-    return performance.now() - past;
   }
 
   function update() {
